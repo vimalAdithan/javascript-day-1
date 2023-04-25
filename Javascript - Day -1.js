be1f1d98c8dd6f402dbe895db8edc59e54b1a9d8
Vimal <113934415+vimalAdithan@users.noreply.github.com>
2023-04-25 10:04:17 +0530

    Add files via upload

---
+++ Javascript - Day -1.js
@@ -0,0 +1,20 @@
+/*
+HTTP
+Hypertext transfer protocol
+Http is used to send user request and receive responses
+
+HTTP 1.1
+Each request are works with separate individual requests
+It works in textual format.
+Transformation of data is in inline connection 
+ 
+HTTP 2
+Multiple requests are made with a single connection with the server.
+It works in binary format.
+It compresses the request and works with multiple requests.
+
+Object:
+In javascript an object is meant variable to hold like the data which is collected is related to the object name.
+Object is identified with a dot.
+It's a title.
+*/
